483e0e549d65802f58d57bf2fc60d13b60e62228
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-06-16 18:20:54 -0300

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-create table tb_users
-(
-    name         VARCHAR not null,
-    email        VARCHAR not null,
-    password VARCHAR not null
-);
-
-INSERT INTO tb_users (name, email, password) VALUES ('Eudes', 'eudes@gmail.com', 'deusehfiel');
