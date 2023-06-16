35000fcecf71d241a48647b916da91ca7183a182
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-06-16 18:20:46 -0300

    Delete src directory

--- src/main/java/geeksworld/project/repositories/CharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package geeksworld.project.repositories;
-
-import geeksworld.project.models.CharacterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CharacterRepository extends JpaRepository <CharacterEntity, Long> {
-}
