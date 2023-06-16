fc3c8b78fe5b32f068047b9c0140cfad5cffa129
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-06-16 18:19:35 -0300

    Delete DatabaseConfig.java

--- DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories("br.com.geeksworld.project.repository")
-public class DatabaseConfig {
-}
